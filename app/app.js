405e37caa2031cb2b7b80532baee3535efe1d131
jaehyeon <nwlef1229@naver.com>
2022-05-15 23:35:47 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const home = require("./src/home");
 
@@ -9,6 +10,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/",home);
 
 module.exports = app;
